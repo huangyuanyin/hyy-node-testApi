a6aee8a41c6bf72852ca5de5e4229317a9d9fba2
黄原寅 <3443718488@qq.com>
2023-01-31 18:16:28 +0800

    密码加密&&插入新用户&&封装res.cc函数

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.use(cors())
 // 配置解析表单数据的中间件，注意：这个中间件只能解析 application/x-www-form-urlencoded 格式的表单数据
 app.use(express.urlencoded({ extended: false }))
 
+// 响应数据的中间件（在路由之前，封装res.cc函数）
+app.use((req, res, next) => {
+  // status 默认值为1，表示失败的情况
+  res.cc = function (err, status = 1) {
+    res.send({
+      status,
+      messsage: err instanceof Error ? err.message : err
+    })
+  }
+  next()
+})
+
 // 导入并使用用户路由模块
 const userRouter = require('./router/user')
 app.use('/api', userRouter)
--- router_handler/user.js
+++ router_handler/user.js
@@ -1,6 +1,9 @@
 // 导入数据库操作模块
 const db = require('../db/index')
 
+// bcryptjs - 密码加密
+const bcrypt = require('bcryptjs')
+
 /**
  * 在这里定义和用户相关的路由处理函数，供 /router/user.js 模块进行调用
  */
@@ -20,13 +23,34 @@ exports.regUer = (req, res) => {
   db.query(sqlStr, userInfo.username, (err, results) => {
     // 执行 sql 语句失败
     if (err) {
-      return res.send({ status: 1, message: err.message })
+      // return res.send({ status: 1, message: err.message })
+      return res.cc(err)
     }
     // 判断用户名是否被占用
     if (results.length > 0) {
-      return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+      // return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+      return res.cc('用户名被占用，请更换其他用户名！')
     }
-    // TODO:用户名可以使用
+    // 用户名可以使用
+    // 调用 bcrypt.hashSync() 对密码进行加密
+    userInfo.password = bcrypt.hashSync(userInfo.password, 10)
+
+    // 定义sql语句 - 插入新用户
+    const sql = 'insert into users set ?'
+    db.query(sql, { username: userInfo.username, password: userInfo.password }, (err, results) => {
+      // 判断sql语句是否执行成功
+      if (err) {
+        // return res.send({ status: 1, message: err.message })
+        return res.cc(err)
+      }
+      // 判断影响行数是否为1
+      if (results.affectedRows !== 1) {
+        // return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+        return res.cc('注册用户失败，请稍后再试！')
+      }
+      // 注册用户成功
+      res.send({ status: 0, message: '注册成功！' })
+    })
   })
 }
 
