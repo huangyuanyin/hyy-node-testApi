500bb56c8b72d2509a425a12ec130caac17de3eb
黄原寅 <3443718488@qq.com>
2023-02-01 12:09:55 +0800

    配置解析token的中间件

--- app.js
+++ app.js
@@ -25,14 +25,24 @@ app.use((req, res, next) => {
   next()
 })
 
+// 一定要在路由之前配置解析token的中间件
+// 导入配置文件
+const config = require('./config')
+// 解析token的中间件
+const { expressjwt: expressJWT } = require('express-jwt')
+// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
+app.use(expressJWT({ secret: config.jwtSecretKey, algorithms: ['HS256'] }).unless({ path: [/^\/api\//] }))
+
 // 导入并使用用户路由模块
 const userRouter = require('./router/user')
 app.use('/api', userRouter)
 
 // 定义错误级别的中间件
 app.use((err, req, res, next) => {
-  // 数据验证失败
+  // 数据验证失败导致的错误
   if (err instanceof joi.ValidationError) return res.cc(err)
+  // 身份认证失败后的错误
+  if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
   // 未知的错误
   res.cc(err)
 })
